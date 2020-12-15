1002ab97af2393aa6f482407281078d69ecbe35d
Ollanta CG <ollantaster@gmail.com>
2020-12-15 07:52:31 +0100

    day15: not very inventive

---
+++ day15/memory2.hs
@@ -0,0 +1,30 @@
+import Text.Parsec
+import qualified Data.HashMap.Strict as M
+
+
+main :: IO ()
+main = do
+  interact (show . solve . readD)
+  putStrLn ""
+
+
+readD :: String -> [Int]
+readD s = map read ops
+  where
+    Right ops = parse ((many1 digit) `sepBy` string ",") "" s
+      
+
+solve start = plays !! (30000000 - 1)
+  where
+    plays = start ++ play minit (last start, length start)
+    minit = M.fromList $ zip mstart [1..]
+      where
+        mstart = reverse . tail . reverse $ start
+
+    play m (p, i)
+      | not (M.member p m) = 0 : play m' (0, i+1)
+      | otherwise          = p': play m' (p', i+1)
+      where
+        p' = i - m M.! p
+        m' = M.insert p i m
+    
