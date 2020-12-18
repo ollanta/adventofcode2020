43f860de7d785897f46319837f612cd707e9a2ad
Ollanta CG <ollantaster@gmail.com>
2020-12-18 13:21:37 +0100

    day18: more direct parsec solution

---
+++ day18/math3.hs
@@ -0,0 +1,30 @@
+import Text.Parsec
+import Text.Parsec.Expr
+
+
+main :: IO ()
+main = do
+  interact (show . (\s -> (solve1 s, solve2 s)))
+  putStrLn ""
+
+
+solve1 :: String -> Integer
+solve1 s = solveWithTable [[plusOp, multOp]] s
+
+solve2 :: String -> Integer
+solve2 s = solveWithTable [[plusOp], [multOp]] s
+
+solveWithTable table s = sum rs
+  where
+    Right rs = parse (expr `endBy` newline) "" (filter (/=' ') s)
+
+    expr    = buildExpressionParser table term
+  
+    term = between (string "(") (string ")") expr
+       <|> integer
+
+    integer = read <$> many1 digit
+
+multOp = Infix (string "*" >> pure (*)) AssocLeft
+
+plusOp = Infix (string "+" >> pure (+)) AssocLeft
